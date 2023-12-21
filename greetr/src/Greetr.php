30f04051fdb288e29db828315e814ffbee1c3e56
HossainRifat <rh140025@gmail.com>
2023-12-21 23:04:14 +0600

    first commit

---
+++ greetr/src/Greetr.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Simplexi\Greetr;
+
+class Greetr
+{
+    public function greet(String $name)
+    {
+        return "Hello {$name}! Welcome to Simplexi! This is a test package!";
+    }
+}
